b7fd61a9a9fdedf0e8c8e9a5835b807d1330f5cf
David Wronek <david@mainlining.org>
2024-06-12 21:07:40 +0200

    samsung-sm8650-prepare: Add android_device_qcom_sepolicy_vndr

--- samsung-sm8650-prepare.sh
+++ samsung-sm8650-prepare.sh
@@ -42,14 +42,15 @@ repopick 389365 394099 394100
 
 # Clone sm8650 HALs
 [[ $USE_SSH -eq 1 ]] && note "Using SSH to clone git repositories." || note "Using HTTPS to clone git repositores."
+dl-src android_device_qcom_sepolicy_vndr lineage-21.0-caf-sm8650 device/qcom/sepolicy_vndr/sm8650
 dl-src android_device_samsung_e3q lineage-21 device/samsung/e3q
 dl-src android_hardware_qcom_audio-ar lineage-21.0-caf-sm8650 hardware/qcom-caf/sm8650/audio/primary-hal
 dl-src android_hardware_qcom_display lineage-21.0-caf-sm8650 hardware/qcom-caf/sm8650/display
 dl-src android_hardware_qcom_media lineage-21.0-caf-sm8650 hardware/qcom-caf/sm8650/media
 dl-src android_vendor_qcom_opensource_agm lineage-21.0-caf-sm8650 hardware/qcom-caf/sm8650/audio/agm
 dl-src android_vendor_qcom_opensource_arpal-lx lineage-21.0-caf-sm8650 hardware/qcom-caf/sm8650/audio/pal
-dl-src android_vendor_qcom_opensource_dataipa lineage-21.0-caf-sm8650 hardware/qcom-caf/sm8650/dataipa
 dl-src android_vendor_qcom_opensource_data-ipa-cfg-mgr lineage-21.0-caf-sm8650 hardware/qcom-caf/sm8650/data-ipa-cfg-mgr
+dl-src android_vendor_qcom_opensource_dataipa lineage-21.0-caf-sm8650 hardware/qcom-caf/sm8650/dataipa
 
 # Add necessary symlinks
 ln -s ../../common/os_pickup_audio-ar.mk hardware/qcom-caf/sm8650/audio/Android.mk
